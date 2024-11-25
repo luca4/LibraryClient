4596514e5ab1bf79b5521964575a3e8fcb2fecdf
luca4 <>
2024-11-25 17:49:08 +0100

    refactor code for better error handling and structure

--- library_client.py
+++ library_client.py
@@ -1,4 +1,5 @@
 import time
+from http import HTTPStatus
 
 import requests
 from config import base_url
@@ -13,75 +14,54 @@ class LibraryClient:
 
     def login(self, username: str, password: str):
         body = {'username': username, 'password': password}
-        response = requests.post(f"{base_url}/auth/login", body)
+        response = self._send_request("POST", "/auth/login", data=body)
 
-        if response.status_code == 200:
-            res_data = response.json()
-            self.auth_token = res_data.get("token")
-            self.token_expiration = time.time() + int(res_data.get("expires_in_sec", 0))
-            self.is_admin = res_data.get("is_admin", False).lower() == "true"
-        elif response.status_code == 401:
-            raise AuthenticationError("Wrong username or password")
-        elif response.status_code == 400:
-            raise ValidationError("Something wrong in message sent to server")
-
-    def ensure_authentication(self):
-        if time.time() > self.token_expiration:
-            raise AuthenticationError("Token expired, please log in")
+        res_data = response.json()
+        self.auth_token = res_data.get("token")
+        self.token_expiration = time.time() + int(res_data.get("expires_in_sec", 0))
+        self.is_admin = res_data.get("is_admin", False).lower() == "true"
 
     def get_books(self):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.get(f"{base_url}/books", headers=auth_header)
-
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise LibraryError("Error while retrieving books from server")
+        self._ensure_authentication()
+        response = self._send_request("GET", "/books")
+        return response.json()
 
     def borrow_book(self, book_id: int):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.post(f"{base_url}/books/{book_id}/borrow", headers=auth_header)
-
-        if response.status_code != 200:
-            raise LibraryError(f"Error while borrowing book with id {book_id}")
+        self._ensure_authentication()
+        self._send_request("POST", f"/books/{book_id}/borrow")
 
     def return_book(self, book_id: int):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.post(f"{base_url}/books/{book_id}/return", headers=auth_header)
-
-        if response.status_code != 200:
-            raise LibraryError(f"Error while returning book with id {book_id}")
+        self._ensure_authentication()
+        self._send_request("POST", f"/books/{book_id}/return")
 
     # Admin endpoints #
     def add_book(self, book_data: dict):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.post(f"{base_url}/books", headers=auth_header, data=book_data)
-
-        if response.status_code == 400:
-            raise ValidationError("Wrong book parameters")
-        elif response.status_code != 204:
-            raise LibraryError("Error while adding book")
+        self._ensure_authentication()
+        self._send_request("POST", "/books", data=book_data)
 
     def delete_book(self, book_id: int):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.delete(f"{base_url}/books/{book_id}", headers=auth_header)
-
-        if response.status_code == 404:
-            raise ResourceNotFoundError(f"No book with id {book_id} found on server")
-        elif response.status_code != 204:
-            raise LibraryError(f"Error while deleting book with id {book_id}")
+        self._ensure_authentication()
+        self._send_request("DELETE", f"/books/{book_id}")
 
     def update_book(self, book_id: int, book_data: dict):
-        self.ensure_authentication()
-        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
-        response = requests.put(f"{base_url}/books/{book_id}", headers=auth_header, data=book_data)
+        self._ensure_authentication()
+        self._send_request("PUT", f"/books/{book_id}", data=book_data)
 
-        if response.status_code == 404:
-            raise ResourceNotFoundError(f"No book with id {book_id} found on server")
-        elif response.status_code != 204:
-            raise LibraryError(f"Error while updating book data with id {book_id}")
+    def _ensure_authentication(self):
+        if time.time() > self.token_expiration:
+            raise AuthenticationError("Token expired, please log in")
+
+    def _send_request(self, http_method: str, endpoint: str, data: dict = {}):
+        url = f"{base_url.rstrip('/')}{endpoint}"
+        auth_header = {"Authorization": f"Bearer {self.auth_token}"}
+
+        response = requests.request(http_method, url, headers=auth_header, data=data)
+
+        if response.status_code == HTTPStatus.UNAUTHORIZED:
+            raise AuthenticationError("Unauthorized request")
+        elif response.status_code == HTTPStatus.NOT_FOUND:
+            raise ResourceNotFoundError("Resource not found on server")
+        elif response.status_code >= 400:
+            raise ValidationError(f"Error on request: {response.text}")
+
+        return response
